3105465a4b5c08b9f6680486b017aafc9d054028
suryaprakash <m.suryaprakash95@gmail.com>
2021-02-17 19:15:59 +0530

    first commit

---
+++ python-helloworld.py
@@ -0,0 +1,21 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# some test data for the response.
+hello_world = 'hello World'
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>This is the home page</h1>
+<p>Add /hello for the desired response.</p>'''
+
+# A route to return the desired response.
+@app.route('/hello', methods=['GET'])
+def api_all():
+    return jsonify(hello_world)
+
+
+app.run()
